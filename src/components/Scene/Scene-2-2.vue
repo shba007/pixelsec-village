f9b2713e6dadb5040ef69a9c22f3ba039f3c4d04
Shirsendu Bairagi <shirsendu2001@gmail.com>
2024-08-13 13:08:02 +0530

    fix: screen updated

--- src/components/Scene/Scene-2-2.vue
+++ src/components/Scene/Scene-2-2.vue
@@ -22,19 +22,21 @@ function onComplete() {
 </script>
 
 <template>
-  <External class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-    <div class="relative flex items-center gap-10 justify-center translate-x-11 bg-red-500">
-      <SmartPhone @select="onSelect" />
-      <!-- <Modal title="" description="How will you spend your time on the tram? <br/> Pick more than one." class="relative aspect-square" :isAbsolute="false" /> -->
-      <div class="flex items-center size-full justify-center relative w-[35vw] aspect-square">
-        <img :src="popupSquare" alt="popupSquare" class="absolute left-0 top-0 size-full object-contain" />
-        <p class="text-left text-2xl whitespace-nowrap z-10">
-          How will you<br />
-          spend your time<br />
-          on the tram? <br /><br />
-          Pick more than<br />
-          one.
-        </p>
+  <External class="absolute top-0 left-0 w-dvw h-dvh">
+    <div class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+      <div class="relative flex items-center gap-10 justify-center translate-x-11">
+        <SmartPhone @select="onSelect" />
+        <!-- <Modal title="" description="How will you spend your time on the tram? <br/> Pick more than one." class="relative aspect-square" :isAbsolute="false" /> -->
+        <div class="flex items-center size-full justify-center relative w-[35vw] aspect-square">
+          <img :src="popupSquare" alt="popupSquare" class="absolute left-0 top-0 size-full object-contain" />
+          <p class="text-left text-2xl whitespace-nowrap z-10">
+            How will you<br />
+            spend your time<br />
+            on the tram? <br /><br />
+            Pick more than<br />
+            one.
+          </p>
+        </div>
       </div>
     </div>
   </External>
