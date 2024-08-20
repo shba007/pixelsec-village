d87ae3d64e3af1bfe22350c052cab4adbd49b684
Shirsendu Bairagi <shirsendu2001@gmail.com>
2024-08-20 19:43:58 +0530

    fix: removed button

--- src/App.vue
+++ src/App.vue
@@ -24,12 +24,14 @@ const mainWindow = window
 
 <template>
   <Application :resize-to="mainWindow" :antialias="false">
-    <Loader :resources="resources" :on-resolved="() => {}">
+    <Loader :resources="resources" :on-resolved="() => { }">
       <template #fallback="{ progress }">
-        <Text :x="screenWidth / 2" :y="screenHeight / 2" :anchor="0.5" :style="{ fill: 'white' }" :scale="0.5"> Loading... {{ Math.round(progress * 100) }}% </Text>
+        <Text :x="screenWidth / 2" :y="screenHeight / 2" :anchor="0.5" :style="{ fill: 'white' }" :scale="0.5">
+          Loading... {{ Math.round(progress * 100) }}% </Text>
       </template>
       <template #default>
-        <ScreenMap :is-load="currentScreenIndex === 0 || currentScreenIndex === 2 || currentScreenIndex === 4" :current-screen-index="currentScreenIndex" @close="updateIndex" />
+        <ScreenMap :is-load="currentScreenIndex === 0 || currentScreenIndex === 2 || currentScreenIndex === 4"
+          :current-screen-index="currentScreenIndex" @close="updateIndex" />
         <ScreenStation v-if="currentScreenIndex === 1" @close="updateIndex" />
         <ScreenPark v-if="currentScreenIndex === 3" @close="updateIndex" />
         <ScreenBank v-if="currentScreenIndex === 5" @close="updateIndex" />
@@ -38,7 +40,7 @@ const mainWindow = window
     </Loader>
   </Application>
   <!-- DEBUG -->
-  <button class="fixed right-0 top-0 z-[99999] bg-white p-2" @click="gameStore.toggleHardStop(!hardStop)">HardStop {{ hardStop }}</button>
+  <!-- <button class="fixed right-0 top-0 z-[99999] bg-white p-2" @click="gameStore.toggleHardStop(!hardStop)">HardStop {{ hardStop }}</button> -->
 </template>
 
 <style lang="css">
