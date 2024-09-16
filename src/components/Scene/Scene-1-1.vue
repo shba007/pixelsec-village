222c9642cc92a66fb747e92cda45bac58b74d98e
Shirsendu Bairagi <shirsendu2001@gmail.com>
2024-09-16 23:27:32 +0530

    refactor: font scaling

--- src/components/Scene/Scene-1-1.vue
+++ src/components/Scene/Scene-1-1.vue
@@ -47,8 +47,8 @@ onTick((delta) => {
   rotate.scale = (75 + Math.sin(count) * 15) / 300
 })
 
-const titleText = reactive({ x: 0, y: 25, anchor: 0.5, scale: 0.25, style: { fontFamily: 'LAN', fontSize: 54 * 4, align: 'center', lineHeight: 64 * 4, stroke: 1, strokeThickness: 1 * 4 } })
-const fullscreenText = reactive({ x: 0, y: 25, anchor: 0.5, scale: 0.25, style: { fontFamily: 'INET', fontSize: 44 * 4, align: 'center', lineHeight: 36 * 4, fill: '#7F7F7F' } })
+const titleText = reactive({ x: 0, y: 25, anchor: 0.5, scale: 1, style: { fontFamily: 'LAN', fontSize: 54, align: 'center', lineHeight: 64, stroke: 1, strokeThickness: 1 } })
+const fullscreenText = reactive({ x: 0, y: 25, anchor: 0.5, scale: 1, style: { fontFamily: 'INET', fontSize: 44, align: 'center', lineHeight: 36, fill: '#7F7F7F' } })
 </script>
 
 <template>
@@ -56,16 +56,24 @@ const fullscreenText = reactive({ x: 0, y: 25, anchor: 0.5, scale: 0.25, style: 
     <Sprite :texture="modal.image" :texture-options="textureOptions" :anchor="0.5" :scale="0.5" />
     <template v-if="!isLandscape">
       <Container :x="titleText.x" :y="titleText.y">
-        <Text :y="-300" :anchor="titleText.anchor" :scale="titleText.scale" :style="{ ...titleText.style, strokeThickness: titleText.style.strokeThickness * 2 }"> WELCOME TO\n DATAVILLE </Text>
-        <Text :anchor="titleText.anchor" :scale="titleText.scale" :style="titleText.style"> Where your online\n habits and choices\n will shape the kind\n of house you live\n in. Let's go! </Text>
-        <Text :anchor="fullscreenText.anchor" :scale="fullscreenText.scale" :x="10" :y="330" :style="fullscreenText.style">Full screen</Text>
+        <Text :y="-300" :anchor="titleText.anchor" :scale="titleText.scale"
+          :style="{ ...titleText.style, strokeThickness: titleText.style.strokeThickness * 2 }"> WELCOME TO\n DATAVILLE
+        </Text>
+        <Text :anchor="titleText.anchor" :scale="titleText.scale" :style="titleText.style"> Where your online\n habits
+          and choices\n will shape the kind\n of house you live\n in. Let's go! </Text>
+        <Text :anchor="fullscreenText.anchor" :scale="fullscreenText.scale" :x="10" :y="330"
+          :style="fullscreenText.style">Full screen</Text>
       </Container>
-      <Sprite texture="popupIconRotate" :x="rotate.x" :y="rotate.y" :scale="rotate.scale" :anchor="0.5" cursor="pointer" @pointerdown="handleStart" />
+      <Sprite texture="popupIconRotate" :x="rotate.x" :y="rotate.y" :scale="rotate.scale" :anchor="0.5" cursor="pointer"
+        @pointerdown="handleStart" />
     </template>
     <template v-else>
       <Container :x="0" :y="20">
-        <Text :y="-160" :anchor="titleText.anchor" :scale="titleText.scale" :style="{ ...titleText.style, strokeThickness: titleText.style.strokeThickness * 2 }"> WELCOME TO DATAVILLE </Text>
-        <Text :y="0" :anchor="titleText.anchor" :scale="titleText.scale" :style="titleText.style"> Where your online habits and\n choices will shape the kind\n of house you live in. Let's go!</Text>
+        <Text :y="-160" :anchor="titleText.anchor" :scale="titleText.scale"
+          :style="{ ...titleText.style, strokeThickness: titleText.style.strokeThickness * 2 }"> WELCOME TO DATAVILLE
+        </Text>
+        <Text :y="0" :anchor="titleText.anchor" :scale="titleText.scale" :style="titleText.style"> Where your online
+          habits and\n choices will shape the kind\n of house you live in. Let's go!</Text>
       </Container>
     </template>
   </Container>
