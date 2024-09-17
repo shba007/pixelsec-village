f4b2ab49cdde37efb463efe0fca6634962e2c27d
Shirsendu Bairagi <shirsendu2001@gmail.com>
2024-09-17 17:57:23 +0530

    fix: width=device-width

--- src/App.vue
+++ src/App.vue
@@ -94,7 +94,7 @@ const loadingText = computed(() => ({ x: screenWidth.value / 2, y: screenHeight.
   </Application>
   <!-- DEBUG -->
   <div class="fixed left-0 top-0 z-[99999] flex flex-col gap-2 bg-white p-2">
-    <p>v0.4.25</p>
+    <p>v0.4.26</p>
     <!--     <p>TimelineIndex: {{ gameStore.timelineIndex }}</p>
     <p>ScreenIndex: {{ gameStore.currentScreenIndex }}</p>
     <p>PopupIndex: {{ gameStore.currentPopupIndex }}</p>
