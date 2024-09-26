4068717c03246750d104fac5d5e876e581c619ae
Shirsendu Bairagi <shirsendu2001@gmail.com>
2024-09-26 11:09:36 +0530

    fix: inital button sound

--- src/components/Scene/Scene-1-1.vue
+++ src/components/Scene/Scene-1-1.vue
@@ -15,6 +15,8 @@ const gameStore = useGameStore()
 const { isLandscape } = storeToRefs(gameStore)
 
 function handleStart() {
+  gameStore.playSFXSound('buttonPressDesign')
+
   setTimeout(async () => {
     await gameStore.toggleGameMode(true)
     gameStore.nextTimeline({ id: 1 })
@@ -39,19 +41,21 @@ const fullscreenText = reactive({ x: 0, y: 25, anchor: 0.5, scale: 1, style: { f
 </script>
 
 <template>
-  <AppPopup v-if="!isLandscape" type="portrait" x="center" y="center" :zoom-factor="zoomFactor" :show-button="false">
-    <Container :x="titleText.x" :y="titleText.y">
-      <Text :y="-300" :anchor="titleText.anchor" :scale="titleText.scale" :style="{ ...titleText.style, strokeThickness: titleText.style.strokeThickness * 2 }"> WELCOME TO\nDATAVILLE </Text>
-      <Text :anchor="titleText.anchor" :scale="titleText.scale" :style="titleText.style"> Where your online\nhabits and choices\nwill shape the kind\nof house you live\nin. Let's go! </Text>
-      <Text v-if="!isMobile.apple.phone" :anchor="fullscreenText.anchor" :scale="fullscreenText.scale" :x="10" :y="330" :style="fullscreenText.style">Tap to Full screen</Text>
-      <Text v-else :anchor="fullscreenText.anchor" :scale="fullscreenText.scale" :x="10" :y="330" :style="fullscreenText.style">Swipe up and rotate!</Text>
-    </Container>
-    <Sprite texture="popupIconRotate" :x="rotate.x" :y="rotate.y" :scale="rotate.scale" :anchor="0.5" cursor="pointer" @pointerdown="handleStart" />
-  </AppPopup>
-  <AppPopup v-else type="landscape" x="center" y="center" :zoom-factor="zoomFactor" @next="onNext">
-    <Container :x="0" :y="20">
-      <Text :y="-160" :anchor="titleText.anchor" :scale="titleText.scale" :style="{ ...titleText.style, strokeThickness: titleText.style.strokeThickness * 2 }"> WELCOME TO DATAVILLE </Text>
-      <Text :y="0" :anchor="titleText.anchor" :scale="titleText.scale" :style="titleText.style"> Where your online habits and\nchoices will shape the kind\nof house you live in. Let's go!</Text>
-    </Container>
+  <AppPopup :type="isLandscape ? 'landscape' : 'portrait'" x="center" y="center" :zoom-factor="zoomFactor" :show-button="false" @next="onNext">
+    <template v-if="!isLandscape">
+      <Container :x="titleText.x" :y="titleText.y">
+        <Text :y="-300" :anchor="titleText.anchor" :scale="titleText.scale" :style="{ ...titleText.style, strokeThickness: titleText.style.strokeThickness * 2 }"> WELCOME TO\nDATAVILLE </Text>
+        <Text :anchor="titleText.anchor" :scale="titleText.scale" :style="titleText.style"> Where your online\nhabits and choices\nwill shape the kind\nof house you live\nin. Let's go! </Text>
+        <Text v-if="!isMobile.apple.phone" :anchor="fullscreenText.anchor" :scale="fullscreenText.scale" :x="10" :y="330" :style="fullscreenText.style">Tap to Full screen</Text>
+        <Text v-else :anchor="fullscreenText.anchor" :scale="fullscreenText.scale" :x="10" :y="330" :style="fullscreenText.style">Swipe up and rotate!</Text>
+      </Container>
+      <Sprite texture="popupIconRotate" :x="rotate.x" :y="rotate.y" :scale="rotate.scale" :anchor="0.5" cursor="pointer" @pointerdown="handleStart" />
+    </template>
+    <template v-else type="landscape" x="center" y="center" :zoom-factor="zoomFactor">
+      <Container :x="0" :y="20">
+        <Text :y="-160" :anchor="titleText.anchor" :scale="titleText.scale" :style="{ ...titleText.style, strokeThickness: titleText.style.strokeThickness * 2 }"> WELCOME TO DATAVILLE </Text>
+        <Text :y="0" :anchor="titleText.anchor" :scale="titleText.scale" :style="titleText.style"> Where your online habits and\nchoices will shape the kind\nof house you live in. Let's go!</Text>
+      </Container>
+    </template>
   </AppPopup>
 </template>
