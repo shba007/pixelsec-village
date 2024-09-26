503a1cdde9e33b8da7031406083f7856e34ead05
Shirsendu Bairagi <shirsendu2001@gmail.com>
2024-09-26 11:26:41 +0530

    fix: scene-1-1 button added

--- src/components/Scene/Scene-1-1.vue
+++ src/components/Scene/Scene-1-1.vue
@@ -41,12 +41,12 @@ const fullscreenText = reactive({ x: 0, y: 25, anchor: 0.5, scale: 1, style: { f
 </script>
 
 <template>
-  <AppPopup :type="isLandscape ? 'landscape' : 'portrait'" x="center" y="center" :zoom-factor="zoomFactor" :show-button="false" @next="onNext">
+  <AppPopup :type="isLandscape ? 'landscape' : 'portrait'" x="center" y="center" :zoom-factor="zoomFactor" :show-button="isLandscape" @next="onNext">
     <template v-if="!isLandscape">
       <Container :x="titleText.x" :y="titleText.y">
         <Text :y="-300" :anchor="titleText.anchor" :scale="titleText.scale" :style="{ ...titleText.style, strokeThickness: titleText.style.strokeThickness * 2 }"> WELCOME TO\nDATAVILLE </Text>
         <Text :anchor="titleText.anchor" :scale="titleText.scale" :style="titleText.style"> Where your online\nhabits and choices\nwill shape the kind\nof house you live\nin. Let's go! </Text>
-        <Text v-if="!isMobile.apple.phone" :anchor="fullscreenText.anchor" :scale="fullscreenText.scale" :x="10" :y="330" :style="fullscreenText.style">Tap to Full screen</Text>
+        <Text v-if="!isMobile.apple.phone" :anchor="fullscreenText.anchor" :scale="fullscreenText.scale" :x="10" :y="330" :style="fullscreenText.style">Tap to full screen</Text>
         <Text v-else :anchor="fullscreenText.anchor" :scale="fullscreenText.scale" :x="10" :y="330" :style="fullscreenText.style">Swipe up and rotate!</Text>
       </Container>
       <Sprite texture="popupIconRotate" :x="rotate.x" :y="rotate.y" :scale="rotate.scale" :anchor="0.5" cursor="pointer" @pointerdown="handleStart" />
